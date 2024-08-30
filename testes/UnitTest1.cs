d73fd77cb5c4042a739936bbbdcb8df5c8826a91
techfrd <fernandonascimento9098@gmail.com>
2024-08-30 20:00:51 -0300

    feat: add teste função somar

--- testes/UnitTest1.cs
+++ testes/UnitTest1.cs
@@ -1,10 +1,15 @@
+using tdd.Class;
+
 namespace testes;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData (2, 3, 5)]
+    public void Test1(int val1, int val2, int res)
     {
-
+        Calculadora calc = new Calculadora();
+        int result = calc.Somar(val1,val2);
+        Assert.Equal(res, result);
     }
 }
